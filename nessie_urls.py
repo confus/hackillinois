916adf350635cf46dc9c4d6c35ad7966b14da5a3
Young Li <ylli2@illinois.edu>
2016-02-20 19:38:06 -0600

    Create functions to easily get Nessie API's URLs

---
+++ nessie_urls.py
@@ -0,0 +1,37 @@
+# accounts
+def url_get_all_accounts(api_key):
+    return "http://api.reimaginebanking.com/accounts?key={}".format(api_key)
+
+def url_get_account_aid(account_id, api_key):
+    return "http://api.reimaginebanking.com/accounts/{}?key={}".format(account_id, api_key)
+
+def url_get_account_cid(customer_id, api_key):
+    return "http://api.reimaginebanking.com/customers/{}/accounts?key={}".format(customer_id, api_key)
+
+def url_account_create(account_id, api_key):
+    return "http://api.reimaginebanking.com/customers/{}/accounts?key={}".format(customer_id, api_key)
+
+def url_account_modify(account_id, api_key):
+    return "http://api.reimaginebanking.com/accounts/{}?key={}".format(account_id, api_key)
+
+def url_account_delete(account_id, api_key):
+    return "http://api.reimaginebanking.com/accounts/{}?key={}".format(account_id, api_key)
+
+# purchases
+def url_get_purchases_aid(account_id, api_key):
+    return "http://api.reimaginebanking.com/accounts/{}/purchases?key={}".format(account_id, api_key)
+
+def url_get_purchases_mid(merchant_id, api_key):
+    return "http://api.reimaginebanking.com/merchants/{}/purchases?key={}".format(merchant_id, api_key)
+
+def url_get_purchase_pid(purchase_id, api_key):
+    return "http://api.reimaginebanking.com/purchases/{}?key={}".format(purchase_id, api_key)
+
+def url_purchase_aid(account_id, api_key):
+    return "http://api.reimaginebanking.com/accounts/{}/purchases?key={}".format(account_id, api_key)
+
+def url_purchase_modify(purchase_id, api_key):
+    return "http://api.reimaginebanking.com/purchases/{}?key={}".format(purchase_id, api_key)
+
+def url_purchase_delete(purchase_id, api_key):
+    return "http://api.reimaginebanking.com/purchases/{}?key={}".format(purchase_id, api_key)
